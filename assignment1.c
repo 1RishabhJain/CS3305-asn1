eeb6a55819d05b8f2561768cbe9e8195d486ff3e
Rishabh Jain <rishabhjain0501@gmail.com>
2021-09-23 00:15:31 -0400

    Initial commit

---
+++ assignment1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+/*
+ * CS3305A - Assignment 1
+ * September 27th, 2021
+ * Rishabh Jain
+*/
+
+// Parent process creates a child process, child_1
+
+// Parent process waits for child_1 to complete
+
+// child_1 calls an external program and passes parameters
+
+// Parent process creates another child process, child_2
+
+// child_2 creates a child process, child_2.1
+
+// child_2.1 calls an external program and passes parameters
+
+// Parent process waits for child_1 and child_2 to complete
---
+++ cmake-build-debug/CMakeFiles/CS3305_asn1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS3305_asn1.dir/assignment1.c.o"
+  "CS3305_asn1"
+  "CS3305_asn1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS3305_asn1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
