6949ba3f8aa6beed2ee8c8109c5ee4361f9bb930
Rishabh Jain <rishabhjain0501@gmail.com>
2021-09-27 00:24:44 -0400

    Clean up + comments

--- assignment1.c
+++ assignment1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
 
 /*
  * CS3305A - Assignment 1
@@ -15,6 +15,9 @@
 int main(int argc, char **argv) {
     // Declaring variables of type pid_t
     pid_t pid1, pid2, pid2_1;
+
+    // Exit boolean
+    bool terminate = false;
 
     // Parent process creates a child process, child 1
     pid1 = fork();
@@ -42,11 +45,12 @@ int main(int argc, char **argv) {
             exit(1);
         }
 
-        // Parent (child 2) process
+        // child 2 process
         if (pid2 > 0) {
-            // Parent process (child 2) creates child 2.1
+            // child 2 creates child 2.1
             pid2_1 = fork();
-            // Parent process waits for child 2.1 to complete
+
+            // child 2 process waits for child 2.1 to complete
             wait(NULL);
 
             // Check if the fork was unsuccessful
@@ -59,43 +63,41 @@ int main(int argc, char **argv) {
             if (pid2_1 > 0) {
                 printf("child_2 (PID %d) created child_2.1 (PID %d)\n", pid2, pid2_1);
 
+                // Converting PID to string
                 int length = floor(log10(abs(pid2_1))) + 1;
                 char PID[length];
-
                 sprintf(PID, "%d", pid2_1);
 
-                //printf("PID %s\n", PID);
+                printf("child_2.1 (PID %d) is calling an external program external_program.out and leaving child_2\n", pid2_1);
 
-                printf("child_2.1 (PID %d) is calling an external program external_program.out and leaving child_2\n",
-                       pid2_1);
-
-                pid2_1 = -1;
+                // Setting boolean terminate to true
+                terminate = true;
 
                 // External program
                 execl(argv[1], "", strcat(PID, " for child_2.1"), NULL);
             }
         }
+
+        // Print termination statement
+        if (terminate) {
+            printf("child_1 and child_2 are completed and parent process is terminating...\n");
+        }
+
         // Child 2
         if (pid2 == 0) {
             printf("parent (PID %d) created child_2 (PID %d)\n", getppid(), getpid());
-        }
-        if (pid2_1 == -1) {
-            printf("child_1 and child_2 are completed and parent process is terminating...\n");
         }
     }
 
     // Child 1 calls an external program and passes parameters
     if (pid1 == 0) {
         printf("parent (PID %d) created child_1 (PID %d)\n", getppid(), getpid());
-        printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2\n", getppid(),
-               getpid());
+        printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2\n", getppid(), getpid());
 
+        // Converting PID to string
         int length = floor(log10(abs(getpid()))) + 1;
         char PID[length];
-
         sprintf(PID, "%d", getpid());
-
-        //printf("PID %s\n", PID);
 
         printf("child_1 (PID %d) is calling an external program external_program.out and leaving parent\n", getpid());
 
