84e040196602057dae1b74c303bafcb15c0d695c
Rishabh Jain <rishabhjain0501@gmail.com>
2021-09-27 19:09:58 -0400

    fixed print statements and logic

--- assignment1.c
+++ assignment1.c
@@ -3,8 +3,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
-#include <math.h>
-#include <stdbool.h>
 
 /*
  * CS3305A - Assignment 1
@@ -14,94 +12,96 @@
 
 int main(int argc, char **argv) {
     // Declaring variables of type pid_t
-    pid_t pid1, pid2, pid2_1;
+    pid_t pid;
 
-    // Exit boolean
-    bool terminate = false;
+    // Character array for sprintf statements
+    char output[200];
 
     // Parent process creates a child process, child 1
-    pid1 = fork();
-
-    // Parent process waits for child 1 to complete
-    wait(NULL);
+    pid = fork();
 
     // Check if the fork was unsuccessful
-    if (pid1 < 0) {
-        printf("fork unsuccessful");
+    if (pid < 0) {
+        puts("fork unsuccessful");
         exit(1);
     }
 
     // Parent process
-    if (pid1 > 0) {
-        // Parent process creates child 2
-        pid2 = fork();
-
-        // Parent process waits for child 2 to complete
+    if (pid > 0) {
+        // Parent process waits for child 1 to complete
         wait(NULL);
-
-        // Check if the fork was unsuccessful
-        if (pid2 < 0) {
-            printf("fork unsuccessful");
-            exit(1);
-        }
-
-        // child 2 process
-        if (pid2 > 0) {
-            // child 2 creates child 2.1
-            pid2_1 = fork();
-
-            // child 2 process waits for child 2.1 to complete
-            wait(NULL);
-
-            // Check if the fork was unsuccessful
-            if (pid2_1 < 0) {
-                printf("fork unsuccessful");
-                exit(1);
-            }
-
-            // Child2.1 process
-            if (pid2_1 > 0) {
-                printf("child_2 (PID %d) created child_2.1 (PID %d)\n", pid2, pid2_1);
-
-                // Converting PID to string
-                int length = floor(log10(abs(pid2_1))) + 1;
-                char PID[length];
-                sprintf(PID, "%d", pid2_1);
-
-                printf("child_2.1 (PID %d) is calling an external program external_program.out and leaving child_2\n", pid2_1);
-
-                // Setting boolean terminate to true
-                terminate = true;
-
-                // External program
-                execl(argv[1], "", strcat(PID, " for child_2.1"), NULL);
-            }
-        }
-
-        // Print termination statement
-        if (terminate) {
-            printf("child_1 and child_2 are completed and parent process is terminating...\n");
-        }
-
-        // Child 2
-        if (pid2 == 0) {
-            printf("parent (PID %d) created child_2 (PID %d)\n", getppid(), getpid());
-        }
     }
 
     // Child 1 calls an external program and passes parameters
-    if (pid1 == 0) {
-        printf("parent (PID %d) created child_1 (PID %d)\n", getppid(), getpid());
-        printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2\n", getppid(), getpid());
+    if (pid == 0) {
+        sprintf(output, "parent (PID %d) created child_1 (PID %d)", getppid(), getpid());
+        puts(output);
+        sprintf(output, "parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2", getppid(),
+               getpid());
+        puts(output);
 
         // Converting PID to string
-        int length = floor(log10(abs(getpid()))) + 1;
-        char PID[length];
+        char PID[30];
         sprintf(PID, "%d", getpid());
 
-        printf("child_1 (PID %d) is calling an external program external_program.out and leaving parent\n", getpid());
+        sprintf(output, "child_1 (PID %d) is calling an external program external_program.out and leaving parent", getpid());
+        puts(output);
 
         // External program
         execl(argv[1], "", strcat(PID, " for child_1"), NULL);
+    }
+
+    // Parent process creates child 2
+    pid = fork();
+
+    // Check if the fork was unsuccessful
+    if (pid < 0) {
+        puts("fork unsuccessful");
+        exit(1);
+    }
+
+    // Parent process
+    if (pid > 0) {
+        // Parent process waits for child 2 to complete
+        wait(NULL);
+
+        // Print termination statement
+        puts("child_1 and child_2 are completed and parent process is terminating...");
+    }
+
+    // child 2 process
+    if (pid == 0) {
+        sprintf(output, "parent (PID %d) created child_2 (PID %d)", getppid(), getpid());
+        puts(output);
+
+        // child 2 creates child 2.1
+        pid = fork();
+
+        // Check if the fork was unsuccessful
+        if (pid < 0) {
+            puts("fork unsuccessful");
+            exit(1);
+        }
+
+        // child 2 process waits for child 2.1 to complete
+        if (pid > 0) {
+            wait(NULL);
+        }
+
+        // Child2.1 process
+        if (pid == 0) {
+            sprintf(output, "child_2 (PID %d) created child_2.1 (PID %d)", getppid(), getpid());
+            puts(output);
+
+            // Converting PID to string
+            char PID[30];
+            sprintf(PID, "%d", getpid());
+
+            sprintf(output, "child_2.1 (PID %d) is calling an external program external_program.out and leaving child_2", getpid());
+            puts(output);
+
+            // External program
+            execl(argv[1], "", strcat(PID, " for child_2.1"), NULL);
+        }
     }
 }
